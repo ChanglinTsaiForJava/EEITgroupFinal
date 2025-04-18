8185696873d9dedbc3e10cc8c865580d512503aa
steve <changlin.stevetsai@gmail.com>
2025-04-18 16:28:35 +0800

    ****Main**** first push

---
+++ src/test/java/eeit/OldProject/OldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package eeit.OldProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
