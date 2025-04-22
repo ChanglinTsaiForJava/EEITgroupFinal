10be771e018df45f6100213dbd6d04fcecb7a542
steve <changlin.stevetsai@gmail.com>
2025-04-22 12:24:19 +0800

    archive CRUD

--- src/main/java/eeit/OldProject/steve/Repository/Favorite_courseRepository.java
+++ src/main/java/eeit/OldProject/steve/Repository/Favorite_courseRepository.java
@@ -4,8 +4,15 @@ import eeit.OldProject.steve.Entity.Favorite_course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface Favorite_courseRepository extends JpaRepository<Favorite_course, Long> {
+    // 根據使用者和課程ID查詢收藏
+    Optional<Favorite_course> findByUserIdAndCourseId(Long userId, Long courseId);
 
+    // 根據使用者ID查詢所有收藏課程
+    List<Favorite_course> findByUserId(Long userId);
 }
