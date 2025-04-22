ff9f70e75ff584a08ca634e88e84ec9f68178905
cozyunicorn <cozyunicorn@gmail.com>
2025-04-22 12:47:35 +0800

    fixed recordRepo

--- src/main/java/eeit/OldProject/yuni/repository/RecordRepository.java
+++ src/main/java/eeit/OldProject/yuni/repository/RecordRepository.java
@@ -1,7 +1,9 @@
 package eeit.OldProject.yuni.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import eeit.OldProject.yuni.entity.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Integer> {
