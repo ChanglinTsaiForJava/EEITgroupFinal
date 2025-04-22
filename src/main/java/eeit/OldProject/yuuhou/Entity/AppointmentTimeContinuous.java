5286afa9f868fc0685ec83d4ea9949ac39e6eb8d
YuuHou <YuuHou@gmail.com>
2025-04-22 14:13:07 +0800

    yuuhou3.1

--- src/main/java/eeit/OldProject/yuuhou/Entity/AppointmentTimeContinuous.java
+++ src/main/java/eeit/OldProject/yuuhou/Entity/AppointmentTimeContinuous.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CaregiversAppointment {
+public class AppointmentTimeContinuous {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
