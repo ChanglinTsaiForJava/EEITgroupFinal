e019cd564701f1ca42f64313278daccd40d6385b
daniel <d361120041@yahoo.com.tw>
2025-04-25 16:59:21 +0800

    解決有問題的地方

--- src/main/java/eeit/OldProject/rita/Service/NotificationServiceRename.java
+++ src/main/java/eeit/OldProject/rita/Service/NotificationServiceRename.java
@@ -1,27 +1,27 @@
-package eeit.OldProject.rita.Service;
-
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class NotificationService {
-
-    private final JavaMailSender mailSender;
-
-    public NotificationService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(to);
-        mail.setSubject(subject);
-        mail.setText(body);
-        mail.setFrom("your_email@example.com"); // 記得改信箱
-
-        mailSender.send(mail);
-        System.out.println("📬 已寄出 Email 給：" + to);
-
-    }
-}
+//package eeit.OldProject.rita.Service;
+//
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class NotificationServiceRename {
+//
+//    private final JavaMailSender mailSender;
+//
+//    public NotificationService(JavaMailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
+//
+//    public void sendEmail(String to, String subject, String body) {
+//        SimpleMailMessage mail = new SimpleMailMessage();
+//        mail.setTo(to);
+//        mail.setSubject(subject);
+//        mail.setText(body);
+//        mail.setFrom("your_email@example.com"); // 記得改信箱
+//
+//        mailSender.send(mail);
+//        System.out.println("📬 已寄出 Email 給：" + to);
+//
+//    }
+//}
--- src/main/java/eeit/OldProject/yuuhou/Config/SecurityConfig.java
+++ src/main/java/eeit/OldProject/yuuhou/Config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/caregiver/**").hasRole("CAREGIVER")
                 .requestMatchers("/api/caregivers").permitAll()
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest().authenticated()
             .and()
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
