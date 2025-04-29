30076b36ded56103af714caf11376ecd8a6d1581
Allen <allen@gamil.com>
2025-04-29 09:09:07 +0800

    暫時移除公告後端

--- src/main/java/eeit/OldProject/allen/Controller/AnnouncementAdminController.java
+++ src/main/java/eeit/OldProject/allen/Controller/AnnouncementAdminController.java
@@ -1,46 +1,5 @@
 package eeit.OldProject.allen.Controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import eeit.OldProject.allen.Entity.Announcement;
-import eeit.OldProject.allen.Service.AnnouncementService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
-@RestController
-@RequestMapping("/announcement/admin")
 public class AnnouncementAdminController {
-	@Autowired
-	private AnnouncementService announcementService;
-	
-	//查詢一筆公告
-	@GetMapping("/{id}")
-	public Announcement getAnnouncementById(@PathVariable Integer id) {
-		Announcement announcement = announcementService.findById(id);
-		if(announcement == null) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "找不到這筆公告");
-		}
-		return announcement;
-	}
-
-	//查詢所有公告
-	@GetMapping
-	public List<Announcement> getAllAnnouncement() {
-		return announcementService.findAll();
-	}
-	
-	 // 新增公告
-    @PostMapping
-    public Announcement createAnnouncement(@RequestBody Announcement announcement) {
-        return announcementService.save(announcement);
-    }
 	
 }
--- src/main/java/eeit/OldProject/allen/Service/AnnouncementService.java
+++ src/main/java/eeit/OldProject/allen/Service/AnnouncementService.java
@@ -1,12 +1,5 @@
 package eeit.OldProject.allen.Service;
 
-import java.util.List;
-
-import eeit.OldProject.allen.Entity.Announcement;
-
 public interface AnnouncementService {
-	//基本CRUD
-	Announcement findById(Integer id);
-	List<Announcement> findAll();
-	Announcement save(Announcement announcement);
+	
 }
