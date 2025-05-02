122b6962e95c468881c49bbae348c11c33d7c79d
steve <changlin.stevetsai@gmail.com>
2025-05-02 15:55:43 +0800

    steve 0502

--- src/main/java/eeit/OldProject/SpringBootConfig.java
+++ src/main/java/eeit/OldProject/SpringBootConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringBootConfig implements WebMvcConfigurer {
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+		registry.addMapping("/**") // 全部後端路徑都允許跨域
+				.allowedOrigins("http://localhost:5173") // 前端 localhost:5173
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 }
