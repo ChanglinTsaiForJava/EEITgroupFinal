00339330bf484a4f4bce2ee328354b0007d5e5ad
Allen <allen@gamil.com>
2025-05-12 15:59:54 +0800

    0512-修改

--- src/main/java/eeit/OldProject/SpringBootConfig.java
+++ src/main/java/eeit/OldProject/SpringBootConfig.java
@@ -10,7 +10,8 @@ public class SpringBootConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 全部後端路徑都允許跨域
-				.allowedOrigins("http://localhost:5173") // 前端 localhost:5173
+				.allowedOrigins("http://localhost:5173", "http://192.168.36.96:4173",
+				"http://192.168.36.96:8082") // 前端 localhost:5173  *edit by allen
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") //
 				.allowedHeaders("*")
 				.exposedHeaders("Authorization", "Content-Type")
