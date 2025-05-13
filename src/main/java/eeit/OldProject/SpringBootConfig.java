1073fcf445145a5b42fc08399b6b23af5a7c922b
steve <changlin.stevetsai@gmail.com>
2025-05-13 10:54:34 +0800

    add 4173

--- src/main/java/eeit/OldProject/SpringBootConfig.java
+++ src/main/java/eeit/OldProject/SpringBootConfig.java
@@ -10,7 +10,10 @@ public class SpringBootConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 全部後端路徑都允許跨域
-				.allowedOrigins("http://localhost:5173") // 前端 localhost:5173
+				.allowedOrigins( "http://localhost:5173",  // dev
+						"http://localhost:4173",  // preview 預設
+						"http://localhost:4174",  // 有時 Vite 會自動往上找沒被占用的 port
+						"http://localhost:4175"   ) // 前端 localhost:5173
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") //
 				.allowedHeaders("*")
 				.exposedHeaders("Authorization", "Content-Type")
