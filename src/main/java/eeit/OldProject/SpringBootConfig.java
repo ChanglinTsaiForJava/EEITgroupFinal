287ee25d3a888a01d992f30539830745cb627d75
daniel <d361120041@yahoo.com.tw>
2025-05-19 22:26:57 +0800

    daniel laptop ip 192.168.50.140

--- src/main/java/eeit/OldProject/SpringBootConfig.java
+++ src/main/java/eeit/OldProject/SpringBootConfig.java
@@ -25,9 +25,11 @@ public class SpringBootConfig implements WebMvcConfigurer {
                         "http://192.168.36.156:6173",
 						"http://localhost:4175","http://192.168.36.96:4173",
 						// yuni
-						"http://192.168.36.75:4173","http://192.168.36.75:5173"
+						"http://192.168.36.75:4173","http://192.168.36.75:5173",
 //						Rita
-//						"http://192.168.66.77:4173","http://localhost:5173","http://192.168.66.77:5173"
+//						"http://192.168.66.77:4173","http://localhost:5173","http://192.168.66.77:5173",
+						// daniel
+						"http://192.168.50.140:4173", "http://192.168.50.140:5173"
 						) // 前端 localhost:5173.
 				//自行加入部署網域
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") //
