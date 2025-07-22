32411eea8f8db7a9750b4ea14cc73207ff434201
Rita Hung <hys910705@gmail.com>
2025-07-23 00:56:30 +0800

    7/22 update

--- src/main/java/eeit/OldProject/rita/Repository/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package eeit.OldProject.rita.Repository;
-
-import eeit.OldProject.rita.Entity.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-}
